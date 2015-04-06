f8a0c623a09a8965668f624d17d326bec719ccda
leechannl <leechannl@gmail.com>
2015-04-06 17:37:13 +0800

    First blood.

---
+++ DemoForiOS8Tests/DemoForiOS8Tests.swift
@@ -0,0 +1,36 @@
+//
+//  DemoForiOS8Tests.swift
+//  DemoForiOS8Tests
+//
+//  Created by mm on 4/6/15.
+//  Copyright (c) 2015 Pirate. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DemoForiOS8Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo for iOS 8
+
+Demo project while reading *Programming iOS 8*.
