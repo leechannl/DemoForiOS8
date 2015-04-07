3c6a78ca0dd78a0e84e8b8e13406760d0354a096
leechannl <leechannl@gmail.com>
2015-04-07 22:43:46 +0800

    Change constraints at run time.

--- DemoForiOS8/ViewController.swift
+++ DemoForiOS8/ViewController.swift
@@ -10,43 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var v1 = UIView()
+    var v2 = UIView()
+    var v3 = UIView()
+    var constraintsWith = [NSLayoutConstraint]()
+    var constraintsWithout = [NSLayoutConstraint]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let v1 = UIView(frame: CGRectMake(100.0, 111.0, 132.0, 194.0))
-        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        let v1 = UIView()
+        v1.backgroundColor = UIColor.redColor()
+        v1.setTranslatesAutoresizingMaskIntoConstraints(false)
         let v2 = UIView()
-        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView()
-        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
-
-        self.view.addSubview(v1)
-        v1.addSubview(v2)
-        v1.addSubview(v3)
+        v2.backgroundColor = UIColor.yellowColor()
         v2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let v3 = UIView()
+        v3.backgroundColor = UIColor.blueColor()
         v3.setTranslatesAutoresizingMaskIntoConstraints(false)
 
-        let d = ["v2": v2, "v3": v3];
-        v1.addConstraints(
-            NSLayoutConstraint.constraintsWithVisualFormat("H:|[v2]|", options: nil, metrics: nil, views: d)
-        )
-        v1.addConstraints(
-            NSLayoutConstraint.constraintsWithVisualFormat("V:|[v2(10)]", options: nil, metrics: nil, views: d)
-        )
-        v1.addConstraints(
-            NSLayoutConstraint.constraintsWithVisualFormat("H:[v3(20)]|", options: nil, metrics: nil, views: d)
-        )
-        v1.addConstraints(
-            NSLayoutConstraint.constraintsWithVisualFormat("V:[v3(20)]|", options: nil, metrics: nil, views: d)
-        )
+        self.view.addSubview(v1)
+        self.view.addSubview(v2)
+        self.view.addSubview(v3)
+
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+
+        let c1 = NSLayoutConstraint.constraintsWithVisualFormat("H:|-(20)-[v(100)]", options: nil, metrics: nil, views: ["v": v1]) as [NSLayoutConstraint]
+        let c2 = NSLayoutConstraint.constraintsWithVisualFormat("H:|-(20)-[v(100)]", options: nil, metrics: nil, views: ["v": v2]) as [NSLayoutConstraint]
+        let c3 = NSLayoutConstraint.constraintsWithVisualFormat("H:|-(20)-[v(100)]", options: nil, metrics: nil, views: ["v": v3]) as [NSLayoutConstraint]
+        let c4 = NSLayoutConstraint.constraintsWithVisualFormat("V:|-(100)-[v(20)]", options: nil, metrics: nil, views: ["v": v1]) as [NSLayoutConstraint]
+        let c5with = NSLayoutConstraint.constraintsWithVisualFormat("V:[v1]-(20)-[v2(20)]-(20)-[v3(20)]", options: nil, metrics: nil, views: ["v1":v1, "v2": v2, "v3": v3]) as [NSLayoutConstraint]
+        let c5without = NSLayoutConstraint.constraintsWithVisualFormat("V:[v1]-(20)-[v3(20)]", options: nil, metrics: nil, views: ["v1": v1, "v3": v3]) as [NSLayoutConstraint]
+
+        self.constraintsWith.extend(c1)
+        self.constraintsWith.extend(c2)
+        self.constraintsWith.extend(c3)
+        self.constraintsWith.extend(c4)
+        self.constraintsWith.extend(c5with)
+
+        self.constraintsWithout.extend(c1)
+        self.constraintsWithout.extend(c3)
+        self.constraintsWithout.extend(c4)
+        self.constraintsWithout.extend(c5without)
+
+        NSLayoutConstraint.activateConstraints(self.constraintsWith)
+
+        var button = UIButton(frame: CGRectMake(10.0, 10.0, 100.0, 44.0))
+        button.setTitle("switch", forState: .Normal)
+        button.setTitleColor(UIColor.greenColor(), forState: .Normal)
+        button.addTarget(self, action: "buttonPressed", forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func buttonPressed() {
+        if self.v2.superview != nil {
+            self.v2.removeFromSuperview()
+            NSLayoutConstraint.deactivateConstraints(self.constraintsWith)
+            NSLayoutConstraint.activateConstraints(self.constraintsWithout)
+        } else {
+            self.view.addSubview(self.v2)
+            NSLayoutConstraint.deactivateConstraints(self.constraintsWithout)
+            NSLayoutConstraint.activateConstraints(self.constraintsWith)
+        }
     }
-
 
 }
 
